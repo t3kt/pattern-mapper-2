4f753396a36c02e4fb85ab2b0c78c43005e77e20
tekt <tekt@optexture.com>
2019-12-16 22:55:22 -0800

    laying out skeleton for a pattern geometry builder

---
+++ builder/pattern_geometry_builder/PatternGeometryBuilderExt.py
@@ -0,0 +1,12 @@
+import common
+from pm2_model import PPattern, PShape
+from pm2_settings import PPreProcSettings, PSettings, BoundType
+from typing import List
+
+# noinspection PyUnreachableCode
+if False:
+	# noinspection PyUnresolvedReferences
+	from _stubs import *
+
+class PatternGeometryBuilder(common.ExtensionBase):
+	pass
