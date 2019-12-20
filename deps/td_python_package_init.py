833826d31555e7c17ee4d4ba72ba385b61088cbd
tekt <tekt@optexture.com>
2019-12-19 20:57:27 -0800

    attempting to get things to be loadable in an external console

--- deps/td_python_package_init.py
+++ deps/td_python_package_init.py
@@ -5,8 +5,12 @@ if False:
 	# noinspection PyUnresolvedReferences
 	from _stubs import *
 
+
 def init():
-	pkgpath = project.folder + '/deps'
+	proj = globals().get('project')
+	if not proj:
+		return
+	pkgpath = proj.folder + '/deps'
 
 	if pkgpath not in sys.path:
 		sys.path.append(pkgpath)
