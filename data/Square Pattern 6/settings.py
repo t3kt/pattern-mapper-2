d49c0ade15e0d68f521c00b64e7d8e6a697c122b
tekt <tekt@optexture.com>
2019-12-28 15:56:30 -0800

    square pattern 6

---
+++ data/Square Pattern 6/settings.py
@@ -0,0 +1,33 @@
+from pm2_settings import *
+
+def settings():
+	return PSettings(
+		preProc=PPreProcSettings(
+			recenter=PRecenterSettings(bound=BoundType.frame),
+			rescale=PRescaleSettings(bound=BoundType.frame),
+			fixTriangleCenters=True,
+		),
+		dedup=PDuplicateMergeSettings(
+			ignoreDepth=True,
+		),
+		grouping=PGroupingSettings(
+			groupGenerators=
+			[
+				PPathGroupGenSpec(
+					groupName=n,
+					paths=['^svg/g\\[id={}\\]/.*'.format(n.replace('.', '\\.'))],
+					groupAtPathDepth=groupAt,
+				)
+				for n, groupAt in [
+					('1.a', 2),
+					('1.b', 2),
+					('2.a', 2),
+					('2.b', 3),
+					('2.c', 2),
+					('3.a', 4),
+					('3.b', 3),
+					('3.c', 2),
+				]
+			]
+		),
+	)
