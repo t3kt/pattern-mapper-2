b8afcbf4dd51e39c9a0843ecda041d0b42be36d6
tekt <tekt@optexture.com>
2019-12-29 19:58:44 -0800

    Create PatternSequencerExt.py

---
+++ builder/pattern_sequencer/PatternSequencerExt.py
@@ -0,0 +1,12 @@
+import common
+
+# noinspection PyUnreachableCode
+if False:
+	# noinspection PyUnresolvedReferences
+	from _stubs import *
+
+class PatternSequencer(common.ExtensionBase):
+	pass
+
+class _PatternGenerator(common.LoggableSubComponent):
+	pass
