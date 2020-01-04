bdc423d71bbbcca225178150645749296ff53ebb
tekt <tekt@optexture.com>
2020-01-03 21:24:59 -0800

    initial framework for project load/save

--- runtime/RuntimeAppExt.py
+++ runtime/RuntimeAppExt.py
@@ -1,5 +1,9 @@
+from typing import Iterable
+
 import common
-from common import loggedmethod
+from common import loggedmethod, simpleloggedmethod
+from pm2_project import PProject
+from pm2_runtime_shared import RuntimeSubsystem
 
 # noinspection PyUnreachableCode
 if False:
@@ -23,6 +27,27 @@ class RuntimeApp(common.ExtensionBase):
 	@loggedmethod
 	def OnChoosePattern(self):
 		self.op('pattern_loader').par.Extract.pulse()
+		projectJson = self.op('project_json').text or '{}'
+		project = PProject.parseJsonStr(projectJson)
+		self._LoadProject(project)
 		self.ClosePatternChooser()
 		self.ShowUI()
-		# TODO: other stuff?
+
+	def SaveProject(self):
+		project = self._BuildProject()
+
+		raise NotImplementedError()
+
+	def _GetSubSystems(self) -> Iterable[RuntimeSubsystem]:
+		raise NotImplementedError()
+
+	@simpleloggedmethod
+	def _LoadProject(self, project: PProject):
+		for comp in self._GetSubSystems():
+			comp.ReadFromProject(project)
+
+	def _BuildProject(self) -> PProject:
+		project = PProject()
+		for comp in self._GetSubSystems():
+			comp.WriteToProject(project)
+		return project
