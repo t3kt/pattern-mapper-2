bf0be18bf285c58d55436ff9b8e95667a81212d1
tekt <tekt@optexture.com>
2020-01-11 19:23:00 -0800

    add tile mandala 1

---
+++ data/Tile Mandala 1/settings.py
@@ -0,0 +1,28 @@
+from pm2_settings import *
+
+def settings():
+	return PSettings(
+		preProc=PPreProcSettings(
+			recenter=PRecenterSettings(bound=BoundType.frame),
+			rescale=PRescaleSettings(bound=BoundType.frame),
+			fixTriangleCenters=True,
+		),
+		dedup=PDuplicateMergeSettings(
+			ignoreDepth=True,
+		),
+		grouping=PGroupingSettings(
+			groupGenerators=[
+				PIdGroupGenSpec(
+					ids=[
+						'Yellow',
+						'Gold',
+						'Blue',
+						'Teal',
+						'Green',
+						'Purple',
+						'Pink',
+					]
+				)
+			]
+		),
+	)
