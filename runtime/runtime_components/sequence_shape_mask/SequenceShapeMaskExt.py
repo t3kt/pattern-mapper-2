f463a93f5cd03d89f496454f6d755a2020fc3410
tekt <tekt@optexture.com>
2020-01-19 00:10:02 -0800

    shape sequence masks

--- runtime/runtime_components/sequence_shape_mask/SequenceShapeMaskExt.py
+++ runtime/runtime_components/sequence_shape_mask/SequenceShapeMaskExt.py
@@ -6,4 +6,15 @@ if False:
 	from _stubs import *
 
 class SequenceShapeMask(common.ExtensionBase):
-	pass
+	# @common.loggedmethod
+	def BuildStepMaskTable(self, dat: 'DAT', stepsDat: 'DAT', stepCount: int, shapeCount: int):
+		dat.clear()
+		for stepIndex in range(stepCount):
+			cell = stepsDat[str(stepIndex), 'shapeIndices']
+			# self._LogEvent('step {} cell: {!r}'.format(stepIndex, cell))
+			if cell and cell.val:
+				shapeIndices = set(map(int, cell.val.split(' ')))
+				dat.appendRow([1 if shapeIndex in shapeIndices else 0 for shapeIndex in range(shapeCount)])
+			else:
+				dat.appendRow([0] * shapeCount)
+
