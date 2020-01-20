92fe7cc1b39b1f13542adc3cb668a21b28dcedb2
tekt <tekt@optexture.com>
2020-01-19 19:19:40 -0800

    starting on the component manager system
    will replace the state gen / video source / etc managers

---
+++ runtime/runtime_components/component_manager/ComponentManagerExt.py
@@ -0,0 +1,38 @@
+from typing import List
+
+from common import createFromTemplate, OPAttrs, loggedmethod, simpleloggedmethod
+from pm2_project import PComponentSpec
+from pm2_runtime_shared import RuntimeComponent, SerializableComponentOrCOMP
+
+# noinspection PyUnreachableCode
+if False:
+	# noinspection PyUnresolvedReferences
+	from _stubs import *
+
+class ComponentManager(RuntimeComponent):
+	@property
+	def _Components(self) -> List['SerializableComponentOrCOMP']:
+		return self.ops('contents/comp__*')
+
+	@property
+	def _ComponentsInOrder(self):
+		return list(sorted(self._Components, key=lambda c: c.nodeY))
+
+	def WriteComponentSpecs(self) -> List[PComponentSpec]:
+		comps = self._ComponentsInOrder
+		return [
+			comp.GetComponentSpec()
+			for comp in comps
+		]
+
+	def ReadComponentSpecs(self, specs: List[PComponentSpec]):
+		self.ClearComponents()
+		for spec in specs:
+			self.AddComponent(spec)
+
+	def ClearComponents(self):
+		for comp in self._Components:
+			comp.destroy()
+
+	def AddComponent(self, spec: PComponentSpec):
+		pass
