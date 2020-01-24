7942e86cc3e6c6412c4de454b6d3c929954dff51
tekt <tekt@optexture.com>
2020-01-23 21:49:10 -0800

    attempting to persist window settings... but not quite working

--- runtime/app_settings/AppSettingsExt.py
+++ runtime/app_settings/AppSettingsExt.py
@@ -1,7 +1,7 @@
 import json
 import os.path
 
-from common import loggedmethod, toJson
+from common import loggedmethod, toJson, mergeDicts, excludeKeys
 from pm2_runtime_shared import RuntimeComponent, SerializableParams
 
 # noinspection PyUnreachableCode
@@ -14,8 +14,23 @@ class AppSettings(RuntimeComponent, SerializableParams):
 		RuntimeComponent.__init__(self, ownerComp)
 		SerializableParams.__init__(self, ownerComp)
 
+	@property
+	def _UIWindow(self) -> 'windowCOMP':
+		return op.PMUIWindow
+
 	def GetSettingsJson(self):
-		return toJson(self.GetParDict(), minify=False)
+		uiWindow = self._UIWindow
+		vals = mergeDicts(
+			self.GetParDict(),
+			{
+				'uiWindow': {
+					'w': uiWindow.contentWidth,
+					'h': uiWindow.contentHeight,
+					'x': uiWindow.x,
+					'y': uiWindow.y,
+				}
+			})
+		return toJson(vals, minify=False)
 
 	@loggedmethod
 	def SaveSettings(self):
@@ -28,5 +43,15 @@ class AppSettings(RuntimeComponent, SerializableParams):
 			dat = self.op('settings_filein')
 			dat.par.refreshpulse.pulse()
 			settingsJson = dat.text
-		settingsDict = json.loads(settingsJson or '{}')
-		self.SetParDict(settingsDict)
+		vals = json.loads(settingsJson or '{}')
+		self.SetParDict(excludeKeys(vals, ['uiWindow']))
+		uiWindowVals = vals.get('uiWindow') or {}
+		uiWindow = self._UIWindow
+		if 'x' in uiWindowVals:
+			uiWindow.par.winoffsetx = uiWindowVals['x']
+		if 'y' in uiWindowVals:
+			uiWindow.par.winoffsety = uiWindowVals['y']
+		if 'w' in uiWindowVals:
+			uiWindow.par.winw = uiWindowVals['w']
+		if 'h' in uiWindowVals:
+			uiWindow.par.winh = uiWindowVals['h']
