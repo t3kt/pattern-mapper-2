961914247a1f77ff00e22dca511f6e69251226ca
tekt <tekt@optexture.com>
2020-01-31 22:07:36 -0800

    starting the parameter binding system

---
+++ runtime/runtime_components/parameter_bind_host/ParameterBindHostExt.py
@@ -0,0 +1,13 @@
+from common import ExtensionBase
+
+# noinspection PyUnreachableCode
+if False:
+	# noinspection PyUnresolvedReferences
+	from _stubs import *
+
+class ParameterBindHost(ExtensionBase):
+	def Reset(self):
+		pass
+
+	def RegisterComponent(self, comp: 'COMP', key: str):
+		pass
