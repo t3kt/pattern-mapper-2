a659d035aba7fc346d40a630825b78dd0dff0687
tekt <tekt@optexture.com>
2020-02-02 17:40:08 -0800

    added dedup test pattern

---
+++ data/Dedup Test Pattern/settings.py
@@ -0,0 +1,14 @@
+from pm2_settings import *
+
+def settings():
+	return PSettings(
+		preProc=PPreProcSettings(
+			recenter=PRecenterSettings(bound=BoundType.frame),
+			rescale=PRescaleSettings(bound=BoundType.frame),
+			fixTriangleCenters=True,
+		),
+		dedup=PDuplicateMergeSettings(
+			ignoreDepth=True,
+			equivalence=ShapeEquivalence.alignedPoints,
+		),
+	)
