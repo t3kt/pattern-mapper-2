07873ab53c03132fdb31fd944b6a7f618eb9e629
tekt <tekt@optexture.com>
2020-02-02 17:45:01 -0800

    starting on some ui infrastructure

--- lib/pm2_ui.py
+++ lib/pm2_ui.py
@@ -1,13 +1,14 @@
 from abc import ABC
-from typing import Any
+from dataclasses import dataclass
+from typing import Any, Optional
 
 from common import ExtensionBase
 from pm2_messaging import MessageHandler, MessageSender, Message
 
-# # noinspection PyUnreachableCode
-# if False:
-# 	# noinspection PyUnresolvedReferences
-# 	from _stubs import *
+# noinspection PyUnreachableCode
+if False:
+	# noinspection PyUnresolvedReferences
+	from _stubs import *
 
 class UIApp(ExtensionBase, MessageHandler, MessageSender, ABC):
 	def SendMessage(self, name: str, data: Any = None, namespace: str = None):
@@ -29,3 +30,32 @@ class UISubSystem(UIComponentBase, MessageHandler, MessageSender, ABC):
 			data,
 			namespace or str(self.par.Messagenamespace),
 		)
+
+@dataclass
+class MarkerObject:
+	objectType: str
+	name: str
+
+class Marker(ExtensionBase):
+	def __init__(self, ownerComp):
+		super().__init__(ownerComp)
+		self._InitParams()
+
+	def _InitParams(self):
+		if not hasattr(self.ownerComp.par, 'Markerobjtype'):
+			page = self.ownerComp.appendCustomPage('Marker')
+			page.appendStr('Markerobjtype', label=':Marker Type')
+		if not hasattr(self.ownerComp.par, 'Markerobjname'):
+			page = self.ownerComp.appendCustomPage('Marker')
+			page.appendStr('Markerobjtype', label=':Marker Name')
+
+	def GetMarkerObject(self) -> Optional[MarkerObject]:
+		obj = MarkerObject(self.par.Markerobjtype.eval(), self.par.Markerobjname.eval())
+		if obj.objectType and obj.name:
+			return obj
+
+def GetMarkerObjectFromOP(comp: 'OP') -> Optional[MarkerObject]:
+	if hasattr(comp, 'GetMarkerObject'):
+		return comp.GetMarkerObject()
+	if hasattr(comp.ext, 'Marker'):
+		return comp.ext.Marker.GetMarkerObject()
