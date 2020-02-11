aab6c199cb9a70d482cf9f5b26b050227a8e97c7
tekt <tekt@optexture.com>
2020-02-10 23:07:12 -0800

    added tesselation 3 expanded basic

---
+++ data/Tesselation 3 Expanded-basic/settings.py
@@ -0,0 +1,13 @@
+from pm2_settings import *
+
+def settings():
+	return PSettings(
+		preProc=PPreProcSettings(
+			recenter=PRecenterSettings(bound=BoundType.frame),
+			rescale=PRescaleSettings(bound=BoundType.frame),
+			fixTriangleCenters=True,
+		),
+		dedup=PDuplicateMergeSettings(
+			ignoreDepth=True,
+		),
+	)
